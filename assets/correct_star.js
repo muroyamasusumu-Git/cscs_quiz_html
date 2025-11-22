4d79af1792b243b52cd9310aa4c4b76b541e5c13
muroyamasusumu-Git <muroyamasusumu@gmail.com>
2025-11-23 06:54:37 +0900

    update

--- assets/correct_star.js
+++ assets/correct_star.js
@@ -58,7 +58,7 @@
 
     // 問題別の3連正解累計キー
     // 例: "cscs_q_correct_streak3_total:20250926-001"
-    var key = "cscs_q_correct_streak3_total:" + qid;
+    var key = "cscs_correct_streak3_total:" + qid;
     var raw = null;
 
     try {
