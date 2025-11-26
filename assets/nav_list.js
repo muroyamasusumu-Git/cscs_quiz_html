8a3f015369f0b628f50f36710daed45d1a54e1a8
muroyamasusumu-Git <muroyamasusumu@gmail.com>
2025-11-27 04:28:42 +0900

    update

--- assets/nav_list.js
+++ assets/nav_list.js
@@ -1019,10 +1019,18 @@
     var leftCol = document.createElement("div");
     leftCol.className = "nl-left-col";
     leftCol.id = "nl-left-col";
+    leftCol.style.marginTop = "6px";
 
     var rightCol = document.createElement("div");
     rightCol.className = "nl-right-col";
     rightCol.id = "nl-right-col";
+    rightCol.style.marginTop = "6px";
+
+    bodyHost.appendChild(leftCol);
+    bodyHost.appendChild(rightCol);
+
+    panel.appendChild(summaryHost);
+    panel.appendChild(bodyHost);
 
     bodyHost.appendChild(leftCol);
     bodyHost.appendChild(rightCol);
