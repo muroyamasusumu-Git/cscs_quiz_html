9e775b9e66f8303c6a6e169682328ad79d0b5685
muroyamasusumu-Git <muroyamasusumu@gmail.com>
2025-12-11 19:49:59 +0900

    update

--- assets/cscs_fade_overlay.js
+++ assets/cscs_fade_overlay.js
@@ -32,7 +32,6 @@
       var cssText =
         "#cscs-fade-highlight-layer, " +
         "#cscs-fade-highlight-layer *{" +
-        "opacity:1 !important;" +
         "transition:none !important;" +
         "transition-property:none !important;" +
         "transition-duration:0s !important;" +
