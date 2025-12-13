3f3b3aadf503f719ca3ec9158b32b564a6d1e1d5
muroyamasusumu-Git <muroyamasusumu@gmail.com>
2025-12-13 21:19:25 +0900

    update

--- assets/cscs_ambient_background.js
+++ assets/cscs_ambient_background.js
@@ -102,12 +102,12 @@
       + "inset: 0;"
       + "pointer-events: none;"
       + "z-index: 0;"
-      // ▼ 楕円の中心を少し左上へ
-      + "transform-origin: 18% 15%;"
+      // 回転の支点を「楕円の中心」に合わせる
+      + "transform-origin: 23% 20%;"
       + "transform: rotate(-5deg);"
       + "background:"
       + "radial-gradient("
-      + "ellipse 1980px 567px at 18% 15%,"
+      + "ellipse 2080px 597px at 23% 20%,"
       + "rgba(58,58,58,0.64) 0%,"
       + "rgba(34,34,34,0.52) 38%,"
       + "rgba(0,0,0,0) 78%"
