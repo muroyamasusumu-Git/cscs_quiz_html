7c0e40b86dca310dc69d9c9c71973831722b37e8
muroyamasusumu-Git <muroyamasusumu@gmail.com>
2025-12-15 04:23:55 +0900

    update

--- assets/nav_list.js
+++ assets/nav_list.js
@@ -681,7 +681,7 @@
         top: "0px",
         zIndex: "100000",
         background: "none",
-        padding: "5px 10px 5px",
+        padding: "0px 10px 5px",
         borderBottomWidth: "1px",
         borderBottomStyle: "solid",
         borderBottomColor: "rgb(42, 42, 42)",
