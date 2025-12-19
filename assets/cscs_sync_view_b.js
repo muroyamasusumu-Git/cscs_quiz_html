6d39d7ea7088253e7d80d491c09c4383080a5a0a
muroyamasusumu-Git <muroyamasusumu@gmail.com>
2025-12-19 19:13:14 +0900

    update

--- assets/cscs_sync_view_b.js
+++ assets/cscs_sync_view_b.js
@@ -140,7 +140,7 @@
     "#cscs_sync_view_b_body .cscs-svb-card {",
     "  background: rgba(0,0,0,0.42);",
     "  border: 1px solid rgba(255,255,255,0.10);",
-    "  border-radius: 8px;",
+    "  border-radius: 10px;",
     "  padding: 10px 10px;",
     "  width: 100%;",
     "  box-sizing: border-box;",
