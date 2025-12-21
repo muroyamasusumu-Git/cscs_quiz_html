30c222e228a56bd32dc8e6dbff0dcfe687c04a36
muroyamasusumu-Git <muroyamasusumu@gmail.com>
2025-12-21 14:14:23 +0900

    update

--- assets/cscs_effects_controller.js
+++ assets/cscs_effects_controller.js
@@ -46,6 +46,7 @@
     { id: "similar_list", label: "SimilarList" },
     { id: "nav_list", label: "NavList" },
     { id: "nl_daily_summary", label: "NlDailySummary" },
+    { id: "star_summary_compact", label: "StarSummaryCompact" },
     { id: "cscs_layout_tuner_fixed", label: "LayoutTuner" },
     { id: "b_wrong_strike_mark", label: "WrongStrike" }
   ];
