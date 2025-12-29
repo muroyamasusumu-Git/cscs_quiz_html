99b83219027a331a0c83f3b91da3932af6a391e4
muroyamasusumu-Git <muroyamasusumu@gmail.com>
2025-12-30 03:57:28 +0900

    update

--- assets/cscs_effects_controller.js
+++ assets/cscs_effects_controller.js
@@ -44,6 +44,7 @@
     { id: "point_summary_board_dummy", label: "PointBoard" },
     { id: "consistency_ng_list", label: "ConsistencyNgList" },
     { id: "field_summary", label: "FieldSummary" },
+    { id: "cscs_state_empty_watch", label: "StateEmptyWatch" },
     { id: "cscs_net_watch", label: "NetWatch" },
     { id: "cscs_debug_trace", label: "DebugTrace" },
     { id: "similar_list", label: "SimilarList" },
