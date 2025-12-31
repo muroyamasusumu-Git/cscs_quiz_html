89c986fd4aa5502baee7b5fb9a0e384ed80fd0f9
muroyamasusumu-Git <muroyamasusumu@gmail.com>
2026-01-01 05:12:02 +0900

    update

--- assets/cscs_sync_a.js
+++ assets/cscs_sync_a.js
@@ -3929,14 +3929,10 @@
 		<div class="sync-card">
 		  <div class="sync-wide-body">
 		    <div class="sync-init-row">
-		      <button type="button" class="sync-toggle-btn" data-sync-init="1">Initキー発行 / 再発行</button>
-		
-		      <span class="sync-mini">
-		        user <span class="sync-user-email">-</span>
-		      </span>
 		      <span class="sync-mini">
 		        key <span class="sync-key-status">-</span>
 		      </span>
+		      <button type="button" class="sync-toggle-btn" data-sync-init="1">Initキー発行 / 再発行</button>
 		      <span class="sync-mini">
 		        state <span class="sync-state-status">-</span>
 		      </span>
